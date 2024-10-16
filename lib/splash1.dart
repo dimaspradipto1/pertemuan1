276bee1377ee035742e5966ae87d3998d74caa16
dimas pradipto <nepsterdms@gmail.com>
2024-10-16 20:23:26 +0700

    feate: homepage

---
+++ lib/splash1.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Splash1 extends StatelessWidget {
+  const Splash1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
